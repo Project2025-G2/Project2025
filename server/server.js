535cc9fae00ff30d5af54bf3243a28b535bddb64
RomaKharitonov616 <147732293+RomaKharitonov616@users.noreply.github.com>
2025-03-24 15:36:14 +0200

    Update server message

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Node.js!' });
+  res.json({ message: 'Hello from updated Node.js!' });
 });
 
 const PORT = 5000;
