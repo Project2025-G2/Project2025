c2667152fdece63e824408d77e11681245d9f60d
RomaKharitonov616 <147732293+RomaKharitonov616@users.noreply.github.com>
2025-04-28 21:05:49 +0300

    Merge pull request #2 from Project2025-G2/feature/home-page
    Feature/home page

---
+++ client/src/data/postersData.js
@@ -0,0 +1,9 @@
+const postersData = [
+    { src: '/images/flow-poster.jpg', alt: 'Flow' },
+    { src: '/images/dragon-poster.jpg', alt: 'How to Train Your Dragon 3' },
+    { src: '/images/monkey-poster.jpg', alt: 'The Monkey' },
+    { src: '/images/captain-America-poster.jpg', alt: 'Captain America' },
+    { src: '/images/true-spider-man-poster.jpg', alt: 'Placeholder' },
+  ];
+  
+  export default postersData;
