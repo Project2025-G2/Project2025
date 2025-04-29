f40acceeddb0d3d0f99817ad86bae5f3b402c1cc
bohuslavstan <bohuslav.stan@gmail.com>
2025-04-29 05:20:42 +0300

    Feat: Create Registration and Login functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Register from './pages/Register';
+import Login from './pages/Login';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
