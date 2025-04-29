989ee70a7a211f4e566d1fa34e4f927c22dead82
9ta1ls <rpeleh19@gmail.com>
2025-04-29 03:25:26 +0300

    Add ESLint configuration and update CI workflow for build process

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+const PORT = 5000
+
 
 dotenv.config({ path: __dirname + '/.env' });
 
@@ -11,7 +13,6 @@ app.get('/api/v1', (req, res) => {
   res.json({ message: 'Hello from Node.js!' })
 })
 
-const PORT = 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   mongoose.connect(process.env.DB_LINK)
